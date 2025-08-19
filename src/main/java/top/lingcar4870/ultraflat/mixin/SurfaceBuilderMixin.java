d0a56e8f0b190284270652469424905eb35c9305
LinGCar <DeluxeLin@outlook.com>
2025-08-19 12:34:10 +0800

    Build terracotta in badlands

--- src/main/java/top/lingcar4870/ultraflat/mixin/SurfaceBuilderMixin.java
+++ src/main/java/top/lingcar4870/ultraflat/mixin/SurfaceBuilderMixin.java
@@ -3,9 +3,11 @@ package top.lingcar4870.ultraflat.mixin;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.math.random.RandomSplitter;
+import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.BlockColumn;
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
@@ -18,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.lingcar4870.ultraflat.worldgen.UltraflatSurfaceBuilder;
 
 @Mixin(SurfaceBuilder.class)
-public class SurfaceBuilderMixin {
+public abstract class SurfaceBuilderMixin {
     @Shadow @Final private DoublePerlinNoiseSampler icebergSurfaceNoise;
     @Shadow @Final private DoublePerlinNoiseSampler icebergPillarNoise;
     @Shadow @Final private DoublePerlinNoiseSampler icebergPillarRoofNoise;
@@ -26,6 +28,11 @@ public class SurfaceBuilderMixin {
     @Shadow @Final private RandomSplitter randomDeriver;
     @Shadow @Final private static BlockState SNOW_BLOCK;
     @Shadow @Final private static BlockState PACKED_ICE;
+    @Shadow @Final private DoublePerlinNoiseSampler badlandsSurfaceNoise;
+    @Shadow @Final private DoublePerlinNoiseSampler badlandsPillarNoise;
+    @Shadow @Final private DoublePerlinNoiseSampler badlandsPillarRoofNoise;
+
+    @Shadow protected abstract BlockState getTerracottaBlock(int x, int y, int z);
 
     @Inject(method = "placeIceberg", at = @At("HEAD"), cancellable = true)
     private void injectPlaceIcebergs(int minY, Biome biome, BlockColumn column, BlockPos.Mutable mutablePos, int x, int z, int surfaceY, CallbackInfo ci) {
@@ -58,12 +65,35 @@ public class SurfaceBuilderMixin {
             for (int o = Math.max(surfaceY, (int)i + 1); o >= minY; o--) {
                 if (column.getState(64).isAir() && 64 < (int) k && random.nextDouble() > 0.01
                         || column.getState(64).isOf(Blocks.WATER) && 64 > (int) j && j != 0.0 && random.nextDouble() > 0.15) {
-                    if (n + 4 <= l && o > m) {
+                    if (n + 5 <= l && o > m) {
                         column.setState(64, SNOW_BLOCK);
                         n++;
                     } else {
                         column.setState(64, PACKED_ICE);
                     }
+                }
+            }
+        }
+        ci.cancel();
+    }
+
+    @Inject(method = "placeBadlandsPillar", at = @At("HEAD"), cancellable = true)
+    private void injectPlaceBadlandsPillar(BlockColumn column, int x, int z, int surfaceY, HeightLimitView chunk, CallbackInfo ci) {
+        if (! ((SurfaceBuilder)(Object) this instanceof UltraflatSurfaceBuilder)) {
+            return;
+        }
+
+        double d = 0.2;
+        double e = Math.min(Math.abs(this.badlandsSurfaceNoise.sample(x, 0.0, z) * 8.25), this.badlandsPillarNoise.sample(x * d, 0.0, z * d) * 15.0);
+        if (!(e <= 0.0)) {
+            double f = 0.75;
+            double g = 1.5;
+            double h = Math.abs(this.badlandsPillarRoofNoise.sample(x * f, 0.0, z * f) * g);
+            double i = 64.0 + Math.min(e * e * 2.5, Math.ceil(h * 50.0) + 24.0);
+            int j = MathHelper.floor(i);
+            if (surfaceY <= j) {
+                if (!column.getState(64).isOf(Blocks.WATER)) {
+                    column.setState(64, this.getTerracottaBlock(x, j, z));
                 }
             }
         }
